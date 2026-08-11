1cc13f1836f7ab6d58a51d61987447c99d5fb6b4
khalyomede <khalyomede@gmail.com>
2026-08-11 16:02:25 +0200

    add installable web app

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,77 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
     plugins: [
         devtools(),
         tailwindcss(),
         solidPlugin(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,png}'],
+                runtimeCaching: [
+                    // Images (Apps icons)
+                    {
+                        handler: "CacheFirst",
+                        urlPattern: /.*\.(webp|avif|png|ico|jpg|jpeg)$/,
+                        method: "GET",
+                    },
+                    // Fonts (Google fonts, Fontawesome, ...)
+                    {
+                        handler: "CacheFirst",
+                        urlPattern: /.*\.(ttf|woff|woff2|otf)$/,
+                        method: "GET",
+                    }
+                ],
+                // Prevent stale/old assets in cache on next load
+                skipWaiting: true,
+                clientsClaim: true,
+                cleanupOutdatedCaches: true,
+            },
+            devOptions: {
+                enabled: false,
+            },
+            manifest: {
+                name: "Kibby",
+                short_name: "Kibby",
+                description: "A cosy word guessing game.",
+                theme_color: "#FFF7ED",
+                dir: "ltr",
+                lang: "en-US",
+                scope: "/",
+                display: "standalone",
+                orientation: "portrait-primary",
+                start_url: "/",
+                id: "play-kibby.com",
+                background_color: "#14532D",
+                categories: [
+                    "games",
+                ],
+                icons: [
+                    {
+                        src: `/icons/kibby-icon-1024.png`,
+                        sizes: "1024x1024",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: `/icons/kibby-icon-512.png`,
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: `/icons/kibby-icon-192.png`,
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                ],
+            }
+        }),
     ],
     server: {
         port: 3000,
