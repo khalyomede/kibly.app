80b965c4455acb1dc8e47ae9088d28131313e91e
khalyomede <khalyomede@gmail.com>
2026-08-12 10:26:36 +0200

    add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
+import { init as initSentry } from "@sentry/solid";
+import { solidRouterBrowserTracingIntegration, withSentryRouterRouting } from "@sentry/solid/solidrouter";
 import { Route, Router } from "@solidjs/router";
 import Home from "./pages/Home";
 import Play from "./pages/Play";
 import 'solid-devtools';
 import './index.css';
+
+initSentry({
+    dsn: "https://253ff891c423bb4bffcc46cc77a14e25@o396055.ingest.us.sentry.io/4510897559306240",
+    tracesSampleRate: 0.1, //  Capture 10% of the transactions
+    integrations: [solidRouterBrowserTracingIntegration()],
+});
 
 const root = document.getElementById('root');
 
@@ -14,13 +22,15 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
     );
 }
 
+const SentryRouter = withSentryRouterRouting(Router);
+
 render(
     () => (
-        <Router>
+        <SentryRouter>
             <Route path="/" component={Home} />
             <Route path="/play" component={Play} />
             <Route path="*404" component={Home} />
-        </Router>
+        </SentryRouter>
     ),
     root!
 );
